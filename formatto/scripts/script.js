5fc52880b9417b90cc6696b081137004080c3746
A'Jee Sieka <darkdispo@gmail.com>
2021-08-12 18:40:03 -0400

    Initial restructuring

--- formatto/scripts/script.js
+++ formatto/scripts/script.js
@@ -1,53 +1,85 @@
-function format() {
-  var opener = document.getElementById("opener").value;
-  var closer = document.getElementById("closer").value;
-  var shell = document.getElementById("shell").value;
-  var lastShell = document.getElementById("lastShell").value;
-  var result = "";
-
-  var shellPieces = shell.split("$$");
-  var lastShellPiece = lastShell.split("$$");
-
-  if(document.getElementById("files").value !== null && document.getElementById("files").value !== "") {
-    var objects = document.getElementById("files").files;
-
-    for(var i = 0; i < objects.length; i++) {
-      if(i === (objects.length - 1)) {
-        result += lastShellPiece[0] + objects[i].name + lastShellPiece[1];
-      }
-      else {
-        result += shellPieces[0] + objects[i].name + shellPieces[1];
+window.onload = function() {
+  document.getElementById("files").onchange = function(e) {
+    var files = e.srcElement.files;
+    var list = "";
+    for(var i = 0; i < files.length; i++) {
+      list += files[i].name;
+      if(i < files.length-1) {
+        list += "\n";
       }
     }
-  }
-  else {
-    var source = document.getElementById("copy").value;
-    let delimiter = document.getElementById("delimiter").value;
-    console.log(source);
-    if(delimiter !== "" && delimiter !== null) {
-      console.log("Delimeter: " + delimiter);
-      var objects = source.split(delimiter);
-    }
-    else {
-      if(source.indexOf(",") > -1) {
-        console.log("Delimeter: ,");
-        var objects = source.split(",");
-      }
-      else if(source.indexOf("\n") > -1) {
-        console.log("Delimeter: \\n");
-        var objects = source.split("\n");
-      }
-    }
-
-    for(var i = 0; i < objects.length; i++) {
-      if(i === (objects.length - 1)) {
-        result += lastShellPiece[0] + objects[i] + lastShellPiece[1];
-      }
-      else {
-        result += shellPieces[0] + objects[i] + shellPieces[1];
-      }
-    }
+    document.getElementById("copy").value = list;
   }
 
-  document.getElementById("result").value = opener + result + closer;
+  document.getElementById("exe-rep").onclick = function() {
+    var remove = document.getElementById("rep-remove").value;
+    var insert = document.getElementById("rep-insert").value;
+    document.getElementById("copy").value = regex(
+      document.getElementById("copy").value,
+      [remove, insert],
+      "replace"
+    );
+  }
+
+  document.getElementById("exe-rem").onclick = function() {
+    var remove = document.getElementById("remove").value;
+    document.getElementById("copy").value = regex(
+      document.getElementById("copy").value,
+      remove,
+      "remove"
+    );
+  }
+
+  document.getElementById("format").onclick = function() {
+    var type = document.getElementById("formatType").value;
+    switch(type) {
+      case "1":
+        toArray();
+        console.log("toArray");
+        break;
+      case "2":
+        toCSV();
+    }
+  }
+}
+
+function regex(source, input, action) {
+  switch(action) {
+    case "replace":
+      console.log(source.replace(input[0], input[1]));
+      return source.replace(new RegExp(input[0], "g"), input[1]);
+      break;
+    case "remove":
+      return source.replace(new RegExp(input[0], "g"), "");
+  }
+}
+
+function toArray() {
+  var source = document.getElementById("copy").value;
+  var output = "[";
+  var delim = document.getElementById("delimiter").value;
+  if(delim === "") {
+    delim = "\n";
+  }
+  var items = source.split(delim);
+  for(var i = 0; i < items.length; i++) {
+    output += "\"" + items[i].replace(/"/g, "'") + "\"";
+    if(i < items.length-1) {
+      output += ",";
+    }
+  }
+  output += "]";
+  document.getElementById("result").value = output;
+}
+
+function toCSV() {
+  var source = document.getElementById("copy").value;
+  var output = "";
+  var delim = document.getElementById("delimiter").value;
+  if(delim === "") {
+    delim = "\n";
+  }
+  var items = source.replace(/,/g, " ").split(delim);
+  output += items.toString();
+  document.getElementById("result").value = output;
 }
--- spritelink/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include 'loggedin.php';
-	session_destroy(); // Is Used To Destroy All Sessions
-	/*Or
-	if(isset($_SESSION['login_user']))
-	unset($_SESSION['login_user']);*/
-?>
