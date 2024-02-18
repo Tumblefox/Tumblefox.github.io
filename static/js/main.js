99553775b396a647df4fc8cce4c17e061ae72f94
A'Jee Sieka <darkdispo@gmail.com>
2024-02-18 13:47:47 -0500

    More style updates

--- static/js/main.js
+++ static/js/main.js
@@ -5,25 +5,30 @@
       duration: 1000
     });
 
-    animationTimeline.add({
-      targets: ".intro-card h1",
-      fontSize: "6em"
-    });
+    let animations = [
+      {
+        targets: ".intro-card h1",
+        fontSize: "6em"
+      },
+      {
+        targets: ".intro-card h1 span",
+        // color: "#000000",
+        boxShadow: "7px 7px 0 white",
+        textShadow: "2px 2px 2px #474747"
+      },
+      {
+        targets: ".intro-card p",
+        keyframes: [
+          {rotate: 10, scale: 0.5},
+          {rotate: -10},
+          {rotate: 0, scale: 1}
+        ],
+        duration: 1000
+      }
+    ];
 
-    animationTimeline.add({
-      targets: ".intro-card h1 span",
-      color: "#000000",
-      boxShadow: "7px 7px 0 white"
-    });
-
-    animationTimeline.add({
-      targets: ".intro-card p",
-      keyframes: [
-        {rotate: 10, scale: 0.5},
-        {rotate: -10},
-        {rotate: 0, scale: 1}
-      ],
-      duration: 1000
+    animations.forEach((animation) => {
+      animationTimeline.add(animation);
     });
   });
 })();
