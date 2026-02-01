c5c8f27be7830b42b14eb19cd8a1a846c9e9bdf6
A'Jee Sieka <darkdispo@gmail.com>
2026-02-01 03:54:53 -0500

    Style updates

--- static/js/main.js
+++ static/js/main.js
@@ -2878,8 +2878,7 @@
             scale: [
               { to: "1.14", ease: "outExpo", duration: 600 },
               { to: "1", ease: "outBounce", duration: 800, delay: 100 }
-            ],
-            "text-shadow": "3px 3px 0 #fff"
+            ]
           }
         },
         {
@@ -2889,17 +2888,9 @@
               { to: "1.14", ease: "outExpo", duration: 600 },
               { to: "1", ease: "outBounce", duration: 800, delay: 100 }
             ],
-            "--c1": [
-              { to: "#7a5c1e", ease: "outExpo", duration: 600 },
-              { to: "#60696D", ease: "outExpo", duration: 800, delay: 100 }
-            ],
-            "--c2": [
-              { to: "#f5e6a8", ease: "outExpo", duration: 600 },
-              { to: "#D3D2D5", ease: "outExpo", duration: 800, delay: 100 }
-            ],
-            "--c3": [
-              { to: "#c9a23f", ease: "outExpo", duration: 600 },
-              { to: "#7C7F85", ease: "outExpo", duration: 800, delay: 100 }
+            color: [
+              { to: "var(--pottery)", ease: "outExpo", duration: 600 },
+              { to: "#000", ease: "outExpo", duration: 800, delay: 100 }
             ],
             delay: stagger(100),
             ease: "inOutCirc"
